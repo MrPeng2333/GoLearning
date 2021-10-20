ea8a1e2a8d0079a693361715fa24ca35abcf6aea
mrpeng2333 <1965638087@qq.com>
2021-10-20 20:55:15 +0800

    ch5 to ch9

---
+++ src/ch9/string/string_func_test.go
@@ -0,0 +1,23 @@
+package string_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestStringFn(t *testing.T) {
+	s := "A,B,C"
+	sep := ","
+	parts := strings.Split(s, sep)
+	for _, part := range parts {
+		t.Log(part)
+	}
+
+	t.Log(strings.Join(parts, "-"))
+}
+
+func TestConv(t *testing.T) {
+	s := strconv.Itoa(10)
+	t.Log("str" + s)
+}

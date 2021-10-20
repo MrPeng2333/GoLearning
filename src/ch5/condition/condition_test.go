2d8b046e21da30370402a9ed682a854eee477096
mrpeng2333 <1965638087@qq.com>
2021-10-20 08:25:50 +0800

    array and slice

--- src/ch5/condition/condition_test.go
+++ src/ch5/condition/condition_test.go
@@ -9,3 +9,17 @@ func TestIfMultiSec(t *testing.T) {
 		t.Log(a)
 	}
 }
+
+func TestSwitchMultiCase(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("Even")
+		case 1, 3:
+			t.Log("Odd")
+		default:
+			t.Log("unknow")
+
+		}
+	}
+}

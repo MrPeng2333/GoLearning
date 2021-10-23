54d29ee1fdb2805195b7b4e18867bbd642585f64
mrpeng2333 <1965638087@qq.com>
2021-10-23 20:29:36 +0800

    共享内存并发机制 and CSP并发机制

--- src/ch17/share_mem/share_mem_test.go
+++ src/ch17/share_mem/share_mem_test.go
@@ -1,6 +1,8 @@
 package share_mem
 
 import (
+	"math/rand"
+	"sync"
 	"testing"
 	"time"
 )
@@ -14,4 +16,68 @@ func TestCounter(t *testing.T) {
 	}
 	time.Sleep(1 * time.Second)
 	t.Logf("counter = %d", counter)
+}
+
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second) // 去掉后，程序运行太快，有些协程没有运行完，得不到预期的结果
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
+
+func TestRWMutex(t *testing.T) {
+	var rwm sync.RWMutex
+	var count int
+	for i := 0; i < 5; i++ {
+		go func() {
+			rwm.RLock()
+			t.Log(count)
+			// go func() {
+			// 	rwm.Lock()
+			// 	count = rand.Intn(1000)
+			// 	rwm.Unlock()
+			// }()
+			rwm.RUnlock()
+		}()
+	}
+
+	for i := 0; i < 5; i++ {
+		go func() {
+			rwm.Lock()
+			rand.Seed(time.Now().Unix())
+			count = rand.Intn(100)
+			// t.Log(count)
+			rwm.Unlock()
+		}()
+	}
+	t.Log("test chang")
+	time.Sleep(time.Second * 1)
 }

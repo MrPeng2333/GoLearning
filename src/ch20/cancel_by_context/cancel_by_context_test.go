20aace0a6eb1dbe4d03bea659b5e28d42f76355f
mrpeng2333 <1965638087@qq.com>
2021-10-25 10:08:41 +0800

    context和任务取消

---
+++ src/ch20/cancel_by_context/cancel_by_context_test.go
@@ -0,0 +1,34 @@
+package cancelbycontext
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func isCancelled(ctx context.Context) bool {
+	select {
+	case <-ctx.Done(): // 是否从ctx.Done()上收到消息，收到消息，执行case的语句
+		return true
+	default:
+		return false
+	}
+}
+
+func TestCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	for i := 0; i < 5; i++ {
+		go func(i int, ctx context.Context) {
+			for {
+				if isCancelled(ctx) {
+					break
+				}
+				time.Sleep(time.Millisecond * 5)
+			}
+			fmt.Println(i, "Cancelled")
+		}(i, ctx)
+	}
+	cancel()
+	time.Sleep(time.Second * 1)
+}

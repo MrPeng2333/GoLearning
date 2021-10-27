445158e571ab40b70ac8417e547f90b1046958a8
mrpeng2333 <1965638087@qq.com>
2021-10-27 16:48:55 +0800

    测试

---
+++ src/ch34/unit_test/functions_test.go
@@ -0,0 +1,40 @@
+package unittest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSquare(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		if ret != expected[i] {
+			t.Errorf("input is %d, the expected is %d, the actual is %d", inputs[i], expected[i], ret)
+		}
+	}
+}
+
+func TestErrorInCode(t *testing.T) {
+	fmt.Println("start")
+	t.Error("Error")
+	fmt.Println("end")
+}
+
+func TestFatalInCode(t *testing.T) {
+	fmt.Println("start")
+	t.Fatal("Fatal")
+	fmt.Println("end")
+}
+
+func TestSquareWithAssert(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		assert.Equal(t, expected[i], ret)
+	}
+}

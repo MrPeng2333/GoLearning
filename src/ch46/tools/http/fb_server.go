7231b0886ad4bbc124d0fa9f4b6d67e509871f95
mrpeng2333 <1965638087@qq.com>
2021-11-04 09:36:52 +0800

    性能分析

---
+++ src/ch46/tools/http/fb_server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+)
+
+func GetFibonacciSerie(n int) []int {
+	ret := make([]int, 2, n)
+	ret[0] = 1
+	ret[1] = 1
+	for i := 2; i < n; i++ {
+		ret = append(ret, ret[i-2]+ret[i-1])
+	}
+	return ret
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+func createFBS(w http.ResponseWriter, r *http.Request) {
+	var fbs []int
+	for i := 0; i < 1000000; i++ {
+		fbs = GetFibonacciSerie(50)
+	}
+	w.Write([]byte(fmt.Sprintf("%v", fbs)))
+}
+func main() {
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/fb", createFBS)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
